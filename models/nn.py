13e4b58da573d4dc3836937e88631004c983592a
Courtney Shearer <csheare@clemson.edu>
2018-03-27 15:16:24 -0400

    temp

---
+++ models/nn.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report,confusion_matrix
+
+
+# TODO:
+
+#knn = KNeighborsClassifier())
