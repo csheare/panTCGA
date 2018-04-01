7e644af37fac8666d8f242d6653b3635a37f9ee0
Courtney Shearer <csheare@clemson.edu>
2018-04-01 12:15:53 -0400

    pathway selecting works

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# WELCOME TO THE COOLEST GENETICS DATA SCIENCE PROJECT EVER
+# panTCGA CPSC4300 project
 
+# Notes:
+## This dataset is LARGE, make sure you have like 20GB of free space or use another computer, maybe the palmetto cluster.
+### To save space :), there are two directories in the drive: allfiles and filesforpathwayscript
+### You really only need filesforpathwayscriptif you want a submatrix for a specific pathway,
+### After running the script the <pathwayname>.json file will appear in the pathways folder
+
+# How to generate new pathway subsets:
+## 1)pick a pathway from: gene_dict.json
+## 2) call pathwayselector.py
+### Note: there maybe some genes that need to be manually put into hallmark_id_to_ensembles.json, just give the missing gene a google, example search: <GeneName> ensemble id, then replace the missing gene with the gene name synonym, this maybe "unfun" but fixing 5 genes out of like 150 is not that bad :)
--- scripts/pathwayselector.py
+++ scripts/pathwayselector.py
@@ -12,44 +12,73 @@ pathwayselector.py
 
         Important File Placements:
             These should be in data folder:
-                ensembles_to_hallmark_id.json
-                gene_dict.json
-                hallmark_experiments.txt
-                hallmark_gene_counts.json
+                filesforpathwayscript.tar <of course, un-tar it, it is still like 10GB :> >
 
-                <below data is in drive>
-                sample_class_list.txt
-                panTCGA_float_data.npy
-                gene_list_panTCGA.npy
-                panTCGA_tumor_count_v1.json
-
-example use:  python pathwayselector.py --pathway "HALLMARK_DNA_REPAIR" --output dna_repair.npy
+example use:  python pathwayselector.py --pathway "HALLMARK_DNA_REPAIR" --output "dna_repair.json"
 
 '''
+import numpy as np
+import json
+import os
+import sys, argparse
+
 # returns an array of the RNAseq values across all samples for a gene
 # gene must be in ensemble format
-def getGeneSamples(gene):
-    print("find Genes")
+def getGeneSamples(gid,gene_list,matrix):
+    if gid in gene_list.keys():
+        gene_num = gene_list[str(gid)]
+    else:
+        return None
+    return list(matrix[gene_num])
 
 # returns a list of genes in ensemble format
 # gene_list is a list of genes in hallmark format
+# Note: if there is not a mapping, google "ensemble id for 'KeyError: ' '", and add it to hallmark_id_to_ensembles.json, kinda lame srry
 def convertGenes(gene_list):
-    print("convert Genes")
+    with open("../data/hallmark_id_to_ensembles.json", 'r') as f:
+        conversion_dict = json.load(f)
+    converted_genes = []
+    for gene in gene_list:
+        converted_genes.append(conversion_dict[gene])
+    return converted_genes
 
-#returns a array of the genes in the pathway in ensemble format
+#returns a list of the genes in the pathway in ensemble format
+# step 1
 def getPathwayGenes(pathway):
+    with open("../data/gene_dict.json", 'r') as f:
+        gene_dict = json.load(f)
+    gene_dict[pathway][-1] = gene_dict[pathway][-1].replace("'","")
+    return gene_dict[pathway]
+
 
 #write an array to ofile that is the specified pathway submatrix
 def getSubMatrix(pathway,ofile):
+    pathway_genes = getPathwayGenes(pathway)
+    converted_genes = convertGenes(pathway_genes)
 
+    with open("../data/gene_list_panTCGA.json", 'r') as f:
+        gene_list = json.load(f)
+    matrix = np.load("../data/panTCGA_gct_data_float_v1.npy")
+    samples_per_gene = {}
+    for possible_genes in converted_genes:
+        for gid in possible_genes:
+            samples_per_gene[gid] = getGeneSamples(gid,gene_list,matrix)
+            if(samples_per_gene[gid] is None):
+                del samples_per_gene[gid]
+                continue
+            else:
+                break
+    with open("../data/pathways/" + str(ofile), 'w') as f:
+        json.dump(samples_per_gene,f)
+    #write samples_per_gene to ofile
 
 
 if __name__ == '__main__':
 
-        parser = argparse.ArgumentParser(description='Get Specific Pathway Matrix')
-        parser.add_argument('--pathway', help='pathway name', type=str, required=True)
-        parser.add_argument('--output', help='output file name', type=str, required=True)
+    parser = argparse.ArgumentParser(description='Get Specific Pathway Matrix')
+    parser.add_argument('--pathway', help='pathway name', type=str, required=True)
+    parser.add_argument('--output', help='output file name', type=str, required=True)
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
-        getSubMatric(args.pathway,args.output)
+    getSubMatrix(args.pathway,args.output)
