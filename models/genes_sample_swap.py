a234497158211437173d978b65b65b7978b1cd09
Courtney Shearer <csheare@clemson.edu>
2018-04-10 13:42:01 -0400

    data fun

---
+++ models/genes_sample_swap.py
@@ -0,0 +1,73 @@
+'''
+
+genes_sample_swap.py
+
+        This file has functions that allow you to swap the x and y data values and add the labels in a additional
+        column for the samples
+
+
+        before:         Sample 1 | Sample 2 | Sample 3 ....
+                GeneA  |  ##           ##       ##
+                GeneB  |  ##           ##       ##
+                GeneC  |  ##           ##       ##
+
+        after:            GeneA | GeneB | GeneC .... | SampleLabel(Cancer Type)
+                Sample1  |  ##     ##       ##          ACA
+                Sample2  |  ##     ##       ##          ACA
+                Sample3  |  ##     ##       ##          ACA
+
+
+
+
+'''
+
+import numpy as np
+import json
+import sys, argparse
+
+#This function will swap the axis and return the new dictionary
+# Step 1
+def swap_axis(old_file):
+    with open("../data/pathways/"+str(old_file),'r') as f:
+        old_dict = json.load(f)
+
+    #convert old json to matrix for ease of transpose
+    old_matrix =  np.zeros([len(old_dict),11102])## Hard Code for our data
+    i = 0
+    for key, value in old_dict.items():
+        old_matrix[i] = old_dict[key]
+        i = i + 1
+
+    new_matrix = np.transpose(old_matrix)
+
+    return new_matrix
+
+
+#Step 2
+def add_sample_labels(new_matrix):
+    temp = open("../data/sample_class_list.txt","r")
+    temp = temp.readlines()
+    for i in range(len(temp)):
+        temp[i] = temp[i].split("\t")[1]
+        temp[i] = temp[i].replace("\n","")
+
+    array = np.asarray(temp)
+
+    matrix_with_labels = np.column_stack((new_matrix,array))
+
+    return matrix_with_labels
+
+
+
+# if __name__ == '__main__':
+#
+#     parser = argparse.ArgumentParser(description='Get Specific Pathway Matrix')
+#     parser.add_argument('--old_file', help='pathway name', type=str, required=True)
+#     parser.add_argument('--new_file', help='output file name', type=str, required=True)
+#
+#     args = parser.parse_args()
+#
+#     matrix_with_labels = addSampleLabels(swapAxis(args.old_file))
+#
+#     outputFile = open("../data/cleaned/"+ str(args.new_file),"wb")
+#     np.save(outputFile,matrix_with_labels);
--- models/nn.py
+++ models/nn.py
@@ -1,10 +1,41 @@
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
+import json
+import os
+import sys, argparse
+#from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report,confusion_matrix
+from genes_sample_swap import swap_axis, add_sample_labels
 
+
+'''
+        The program expects a directory path that contains the following formated files:
+
+        pathway : <pathway name>
+        output  : <output file name *must be numpy array>
+
+        Note: This is to prepare the data for two types of analysis: clustering and classification
+
+        example use:  python nn.py --data "dna_repair.json"
+
+
+'''
 
 # TODO:
 
 #knn = KNeighborsClassifier())
+
+def knn_classify(matrix):
+    knn = KNeighborsClassifier()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Get Specific Pathway Matrix')
+    parser.add_argument('--data', help='data file name', type=str, required=True)
+
+    args = parser.parse_args()
+
+    matrix_with_labels = add_sample_labels(swap_axis(args.data))
+    print(matrix_with_labels)
